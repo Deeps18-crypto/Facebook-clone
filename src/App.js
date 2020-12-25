7cb64693d37c2d3c52c9662d736b6623bcf039d9
Deepak <deepsrh1801@gmail.com>
2020-12-25 19:30:53 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Happy</div>;
+  return <div className="App">Happy hacking</div>;
 }
 
 export default App;
